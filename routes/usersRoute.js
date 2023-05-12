137f790f07d95de08492203a019ec21d0ff992d6
ratchapon-p <ratchapon-p@rmutp.ac.th>
2023-05-12 20:28:58 +0700

    commnet

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,13 +1,16 @@
-import express from "express";
-import { registerUserCtrl, loginUserCtrl, getUserProfileCtrl, updateShippingAddressctrl, } from "../controllers/usersCtrl.js";
-import { isLoggedIn } from "../middlewares/isLoggedin.js";
+import exppress from "express";
+import {
+  registerUserCtrl,
+  loginUserCtrl,
+  getUserProfileCtrl,
+  updateShippingAddresctrl,
+} from "../controllers/usersCtrl.js";
+import { isLoggedIn } from "../middlewares/isLoggedIn.js";
 
-
-const userRoutes = express.Router();
+const userRoutes = exppress.Router();
 
 userRoutes.post("/register", registerUserCtrl);
 userRoutes.post("/login", loginUserCtrl);
 userRoutes.get("/profile", isLoggedIn, getUserProfileCtrl);
-userRoutes.put("/update/shipping", isLoggedIn, updateShippingAddressctrl);
-
+userRoutes.put("/update/shipping", isLoggedIn, updateShippingAddresctrl);
 export default userRoutes;
